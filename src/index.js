8cc65c790e318acb0a7df136bed570d7c8fe4318
“Sourav <elsesourav@gmail.com>
2024-04-03 14:52:38 +0530

    utils add files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connect.js";
+import { app } from "app";
 
 dotenv.config({ path: "./env" });
 
-connectDB()
+connectDB().then(() => {
+   app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+   })
+}).catch((error) => {
+   console.error(`MONGODB Failed to connect for ${error}`);
+   process.exit(1);
+});
